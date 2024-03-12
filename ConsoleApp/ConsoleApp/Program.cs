636acb9a91af86a91622c8f76383bf26bbdc3691
s28229 <s28229@pjwstk.edu.pl>
2024-03-12 11:45:20 +0100

    Static method was added

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Modificatiom 1
+
+
+using System.Collections.Specialized;
+using Microsoft.VisualBasic.CompilerServices;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Welcome to console app");
 var x =23;
+var arr = new int[] { 10, 17, 25, 30, 40, 55, 60, 70 };
+double avg = Queryable.Average(arr.AsQueryable());
+Console.WriteLine("Average = "+avg);
