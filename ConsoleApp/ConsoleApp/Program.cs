68318f197294d06424e2eaf63b2a3884c1d5896b
s28229 <s28229@pjwstk.edu.pl>
2024-03-12 11:23:52 +0100

    First modification

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Modification 1 
+//Modificatiom 1
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Welcome to first console app");
-var x = 23;
+
