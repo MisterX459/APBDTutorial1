8c69c9b235aa8e042232a7cd6935dcbf6d68789d
s28229 <s28229@pjwstk.edu.pl>
2024-03-12 11:55:14 +0100

    Finished doing tasks from class

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -3,7 +3,7 @@
 
 
 using System.Collections.Specialized;
-using Microsoft.VisualBasic.CompilerServices;
+
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Welcome to console app");
@@ -11,3 +11,4 @@ var x =23;
 var arr = new int[] { 10, 17, 25, 30, 40, 55, 60, 70 };
 double avg = Queryable.Average(arr.AsQueryable());
 Console.WriteLine("Average = "+avg);
+  
